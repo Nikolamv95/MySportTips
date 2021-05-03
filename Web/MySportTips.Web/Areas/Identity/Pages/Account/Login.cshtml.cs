ec0a7b34f812d1554a5af0c63c15b40c10949884
Nikola Margaritov <nikola.margaritoov@gmail.com>
2021-05-03 17:41:11 +0300

    Log with username

--- Web/MySportTips.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/MySportTips.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -48,6 +48,10 @@ namespace MySportTips.Web.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
@@ -82,7 +86,7 @@ namespace MySportTips.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
